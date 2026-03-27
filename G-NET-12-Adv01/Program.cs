ca0b06272943092171313e75f921169e1abfb9df
marco <marco@DESKTOP-GCUIOA1>
2026-03-28 01:19:37 +0200

    Q05

--- G-NET-12-Adv01/Program.cs
+++ G-NET-12-Adv01/Program.cs
@@ -76,6 +76,17 @@ namespace G_NET_12_Adv01
 
             #endregion
 
-        }
+            #region Q05
+
+            public static T FindMax<T>(T a, T b) where T : IComparable<T>
+             {
+                 if (a.CompareTo(b) > 0)
+                return a;
+                return b;
+             }
+
+            #endregion
+
+    }
     }
 }
