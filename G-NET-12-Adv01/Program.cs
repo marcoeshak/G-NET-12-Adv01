6865d5beb2b0ca34493e03ee1a1c6fbab7197c1a
marco <marco@DESKTOP-GCUIOA1>
2026-03-28 01:32:46 +0200

    Q10

--- G-NET-12-Adv01/Program.cs
+++ G-NET-12-Adv01/Program.cs
@@ -157,7 +157,30 @@ namespace G_NET_12_Adv01
             */
             #endregion
 
+            #region Q10
 
-    }
+            /*
+            public class Sorter<T> where T : IComparable<T>
+                {
+            public void BubbleSort(T[] array)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        // CompareTo available because of constraint
+                        if (array[j].CompareTo(array[j + 1]) > 0)
+                        {
+                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                        }
+                    }
+                }
+            }
+            */
+
+            #endregion
+
+
+        }
     }
 }
