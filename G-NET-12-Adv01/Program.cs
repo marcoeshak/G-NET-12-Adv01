9f39c44d261438c066b814df5999b0345bb50459
marco <marco@DESKTOP-GCUIOA1>
2026-03-28 01:10:41 +0200

    Q01

--- G-NET-12-Adv01/Program.cs
+++ G-NET-12-Adv01/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace G_NET_12_Adv01
+﻿using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+using System.Timers;
+
+namespace G_NET_12_Adv01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            #region Q01
+
+            /*
+            A generic class uses type parameters that are replaced with actual types when you create an instance.The type parameter <T> acts as a placeholder.
+            
+            Why use :
+
+            Type Safety — Compile-time type checking
+            Performance — No boxing/unboxing
+            Code Reuse — One implementation for all types
+
+            */
+
+            #endregion
+
+
+
+
+
         }
     }
 }
